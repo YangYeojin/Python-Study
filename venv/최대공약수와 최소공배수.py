18c83016b688afb8d0285dcf3c1696e4e23628f4
yangyeojin <yangyeojin79@gmail.com>
2020-04-06 17:43:06 +0900

    https://programmers.co.kr/learn/courses/30/lessons/12940

--- venv/최대공약수와 최소공배수.py
+++ venv/최대공약수와 최소공배수.py
@@ -2,8 +2,9 @@ import math
 
 def solution(n, m):
     answer = []
-    #최대공약수
+    # 최대공약수
     answer.append(math.gcd(n,m))
-    #최소공배수
+    # 최소공배수
+    ## //는 소수점 이하를 버리는 나누기 연산자
     answer.append(n*m//math.gcd(n,m))
     return answer
