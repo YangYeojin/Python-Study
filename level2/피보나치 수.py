8cf7137163ca0676778ed32c818bd2a7c1558337
yangyeojin <yangyeojin79@gmail.com>
2020-06-14 16:30:24 +0900

    https://programmers.co.kr/learn/courses/30/lessons/12945

--- level2/피보나치 수.py
+++ level2/피보나치 수.py
@@ -1,7 +1,5 @@
-def solution(n):
+def solution1(n):
     domain = [1, 0]
-    if n == 1:
-        return 1
     i = 2
     while True:
         if i == n:
@@ -9,3 +7,26 @@ def solution(n):
         else:
             domain = [domain[0] + domain[1], domain[0]]
             i += 1
+
+def solution2(n):
+    domain = [1, 0]
+    i = 2
+    while i != n:
+        domain = [domain[0] + domain[1], domain[0]]
+        i += 1
+    return (domain[0] + domain[1]) % 1234567
+
+# solution1이 미세하게 빠른데 메모리는 약간 더 많이 쓰는데 왜죠
+# 테스트 13
+# solution1 120.43, 10.8MB
+# solution2 122.29, 10.6MB
+
+def solution3(n):
+    domain = [1, 0]
+    i = 2
+    while i != n:
+        domain = [sum(domain), domain[0]]
+        i += 1
+    return (domain[0] + domain[1]) % 1234567
+# sum(domain)과 domain[0] + domain[1]이랑 같은 연산일 거라고 생각했는데 sum()이 훨씬 시간을 많이 사용함
+# 테스트 13번 기준 204.29ms, 10.9MB
