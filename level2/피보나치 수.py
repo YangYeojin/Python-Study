9a6827a0a4107764f584d0404bc579b28cc6bc14
yangyeojin <yangyeojin79@gmail.com>
2020-06-14 16:35:26 +0900

    https://programmers.co.kr/learn/courses/30/lessons/12945

--- level2/피보나치 수.py
+++ level2/피보나치 수.py
@@ -27,6 +27,19 @@ def solution3(n):
     while i != n:
         domain = [sum(domain), domain[0]]
         i += 1
-    return (domain[0] + domain[1]) % 1234567
+    return sum(domain) % 1234567
 # sum(domain)과 domain[0] + domain[1]이랑 같은 연산일 거라고 생각했는데 sum()이 훨씬 시간을 많이 사용함
-# 테스트 13번 기준 204.29ms, 10.9MB
+# 테스트 13번 기준 203.88ms, 10.8MB
+
+
+def solution(n):
+    domain = [1, 0]
+    i = 2
+    while True:
+        if i == n:
+            return sum(domain) % 1234567
+        else:
+            domain = [domain[0] + domain[1], domain[0]]
+            i += 1
+# ??? 그런데 1번과 3번을 조합하여 return에서만 sum()을 사용한 게 더 빠름
+# 테스트 13번 기준 120.32ms, 10.8MB
