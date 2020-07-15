5f25d22d702f438f9fd880f9815ea155f5f3edd6
yangyeojin <yangyeojin79@gmail.com>
2020-07-15 22:34:42 +0900

    https://programmers.co.kr/learn/courses/30/lessons/42747

--- level2/H-Index.py
+++ level2/H-Index.py
@@ -1,10 +1,13 @@
+# 인용된 수가 작은 경우부터 고려하며 인용된 수가 그보다 적게 인용된 논문의 편 수를 넘어가면
+# 그보다 적게 인용된 논문의 편 수<len(citations) - h>를 return
+# len(citations) - h는 인용된 횟수가 citations[h]보다 크거나 같은 논문의 수를 의미함
+
 def solution(citations):
     citations = sorted(citations)
     for h in range(len(citations)):
-        print(citations[h], len(citations) - h)
         if citations[h] >= len(citations) - h:
             return len(citations) - h
     return 0
 
 
-print(solution([10, 13, 0, 16, 1, 15]))
+solution([10, 13, 0, 16, 1, 15])
