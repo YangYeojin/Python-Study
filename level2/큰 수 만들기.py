c090074095f0c161f9ab9259fc63a57684c74e95
yangyeojin <yangyeojin79@gmail.com>
2020-07-27 23:16:20 +0900

    https://programmers.co.kr/learn/courses/30/lessons/42883

---
+++ level2/큰 수 만들기.py
@@ -0,0 +1,16 @@
+def solution(number, k):
+    stack = [number[0]]
+    for i in number[1:]:
+        while len(stack) > 0 and stack[-1] < i and k > 0:
+            stack.pop()
+            k -= 1
+        stack.append(i)
+    if k != 0:
+        stack = stack[:-k]
+    answer = "".join(stack)
+    return answer
+
+
+#print(solution("1231234", 3))
+# 테스트 12번 해결용
+print(solution("319999", 3))
