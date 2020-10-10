11dd1503389d37f64956c7e5da50448c0c59ab74
yangyeojin <yangyeojin79@gmail.com>
2020-10-10 21:05:58 +0900

    1008

--- BeyondProgrammers/미로 탈출.py
+++ BeyondProgrammers/미로 탈출.py
@@ -7,31 +7,39 @@ def maze_escape():
     maze = []
     for i in range(N):
         maze += list(map(str, sys.stdin.readline().split()))
-    visit = [[False for col in range(M)] for row in range(N)]
     roadLen = 1
 
-    dirX = [1, 0, -1, 0]
-    dirY = [0, 1, 0, -1]
+    # range 오류 귀찮으니까 그냥 0으로 패딩
+    pMaze = []
+    for i in range(N+2):
+        if i == 0 or i == N+1:
+            pMaze += ['0'*(M+2)]
+        else:
+            pMaze += ['0'+maze[i-1]+'0']
+    visit = [[False for col in range(M+2)] for row in range(N+2)]   # 방문 여부 확인하는 테이블도 패딩한 크기로 그림
+
+    # 이동 방향
+    dirX = [0, 1, -1, 0]
+    dirY = [1, 0, 0, -1]
 
     # dfs ~~ 이제 생각해보니 이건 bfs가 나았으려나?
     def dfs(graph, c, r, visited, cnt):
         visited[r][c] = True
         for k in range(4):
-            if r == (N-1) and c == (M-1):
+            if r == N and c == M:
                 print(cnt)
                 break
-            elif r+dirY[k] == N or c+dirX[k] == M:
-                continue
             elif graph[r+dirY[k]][c+dirX[k]] == '1' and visited[r+dirY[k]][c+dirX[k]] is False:
-                if k == 0 or k == 1:
-                    cnt += 1
-                    dfs(graph, c + dirX[k], r + dirY[k], visited, cnt)
+                cnt += 1
+                dfs(graph, c + dirX[k], r + dirY[k], visited, cnt)
+                if r == N and c == M:
+                    print(cnt)
+                    break
                 else:
                     cnt -= 1
-                    dfs(graph, c+dirX[k], r+dirY[k], visited, cnt)
 
     # 함수 호출
-    dfs(maze, 0, 0, visit, roadLen)
+    dfs(pMaze, 1, 1, visit, roadLen)
 
 
 maze_escape()
